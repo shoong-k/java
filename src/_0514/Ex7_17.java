e38cce968441e338bb44f37147699772b4fe856a
shoong-k <whooshping@gmail.com>
2022-05-14 13:01:11 +0900

    0514

---
+++ src/_0514/Ex7_17.java
@@ -0,0 +1,11 @@
+package _0514;
+
+public class Ex7_17 {
+    Object iv = new Object() {void method() {}};
+    static Object cv = new Object() {void method(){}};
+
+    void myMethod() {
+        Object lv = new Object() {void method(){}};
+    }
+
+}
---
+++ src/_0514/Ex7_19.java
@@ -0,0 +1,16 @@
+package _0514;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class Ex7_19 {
+    public static void main(String[] args) {
+        Button b = new Button("Start");
+        b.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("ActionEvent occurred!!!");
+            }
+        });
+    }
+}
